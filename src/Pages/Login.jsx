d6748215a198347560ed7abf9f02512c031d4018
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-12-24 17:03:51 +0530

    add login desktop

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import styles from "../styles/pages/Login.module.css";
 const Login = () => {
-  return <div>Loasdgin</div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <div className={styles.pagetitle}>
+          <span>
+            <img src="/logo.svg" alt="" />
+          </span>
+          <span className={styles.title}>Stag Investments</span>
+        </div>
+        <div className={styles.links}>About</div>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.container}>
+          <div className={styles.heading}>
+            Sit back and relax while we make money for you.
+          </div>
+          <div className={styles.login}>
+            <div className={styles.loginbtn}>Get Started</div>
+          </div>
+        </div>
+
+        <div className={styles.imgcontainer}>
+          <img src="/illustrations/login.svg" alt="" />
+        </div>
+
+        <div className={styles.footer}></div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
